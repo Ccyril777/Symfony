b008c037d762bb0f2ca47103a74fb0fdb6c450a6
salazhar <canoine.cyril@gmail.com>
2019-11-18 15:27:40 +0100

    second commit

--- src/Controller/DefautController.php
+++ src/Controller/DefautController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-Class DefaultController extends AbstractController
+Class DefautController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
